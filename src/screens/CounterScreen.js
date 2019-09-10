89fe9c6266f7ca5342320321cc043bc04da40b5c
emrahyasart <emrah35@gmail.com>
2019-09-10 13:54:57 +0300

    Added my solution for adding reducer to CounterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,30 +1,37 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const COUNTER_CHANGE = 1;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, counter: state.counter + action.payload };
+    case "DECREMENT":
+      return { ...state, counter: state.counter + action.payload };
+    case "RESET":
+      return { ...state, counter: action.payload };
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
   return (
     <View>
       <Button
         title="Increase"
-        onPress={() => {
-          //DOnt do this!
-          //counter+++
-          //Do not directly modify counter use setCounter
-          setCounter(counter + 1);
-        }}
+        onPress={() => dispatch({ type: "INCREMENT", payload: COUNTER_CHANGE })}
       />
       <Button
         title="Decrease"
-        onPress={() => {
-          setCounter(counter - 1);
-        }}
+        onPress={() =>
+          dispatch({ type: "DECREMENT", payload: -1 * COUNTER_CHANGE })
+        }
       />
       <Button
         title="Reset"
-        onPress={() => {
-          setCounter(0);
-        }}
+        onPress={() => dispatch({ type: "RESET", payload: 0 })}
       />
       <Text>Current Count: {counter}</Text>
     </View>
